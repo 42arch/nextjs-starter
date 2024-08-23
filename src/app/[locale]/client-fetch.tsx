053d46d1d8e15a570dcadb09f373ed4afc8f6ee4
rendan <rendan@range-soft.com>
2024-08-23 18:03:13 +0800

    add api and data fetching functions

---
+++ src/app/[locale]/client-fetch.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import useSWR from 'swr'
+import { Users } from '../types'
+
+const fetcher = async (url: string): Promise<Users> => {
+  const res = await fetch(url)
+  if (!res.ok) throw new Error('Failed to fetch')
+  const data = res.json()
+  return data
+}
+
+function ClientFetch() {
+  const { data, error } = useSWR<Users>('/api/users', fetcher)
+
+  return (
+    <div>
+      <h1>Client Fetch</h1>
+      <ul>
+        {data?.map((user: { id: number; name: string; email: string }) => (
+          <li key={user.id}>
+            {user.name} - {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default ClientFetch
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,31 @@
-import { useTranslations } from 'next-intl'
+import { getTranslations } from 'next-intl/server'
+import ClientFetch from './client-fetch'
 
-export default function Index({
+export default async function Index({
   params: { locale }
 }: {
   params: { locale: string }
 }) {
-  const t = useTranslations('Index')
+  const t = await getTranslations('Index')
 
-  return <h1 className='p-4 font-sans text-3xl font-bold'>{t('title')}</h1>
+  const res = await fetch('http://localhost:3000/api/users', {
+    cache: 'no-store'
+  })
+  const users = await res.json()
+
+  return (
+    <div>
+      <h1 className='p-4 font-sans text-3xl font-bold'>{t('title')}</h1>
+
+      <ul>
+        {users.map((user: { id: number; name: string; email: string }) => (
+          <li key={user.id}>
+            {user.name} - {user.email}
+          </li>
+        ))}
+      </ul>
+
+      <ClientFetch />
+    </div>
+  )
 }
